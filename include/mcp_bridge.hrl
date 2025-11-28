8867a4bc6f31fc6dd0db69eeff42aee0779a9484
Shawn <506895667@qq.com>
2025-11-28 18:01:29 +0800

    chore: version 1.0.1

--- include/mcp_bridge.hrl
+++ include/mcp_bridge.hrl
@@ -1,9 +1,9 @@
 -define(PLUGIN_NAME, "mcp_bridge").
--define(PLUGIN_VSN, "1.0.0").
+-define(PLUGIN_VSN, ?plugin_rel_vsn).
 -define(MCP_VERSION, <<"2024-11-05">>).
 -define(MCP_BRIDGE_INFO, #{
     <<"name">> => <<"emqx_mcp_bridge">>,
-    <<"version">> => <<"1.0.0">>,
+    <<"version">> => <<?plugin_rel_vsn>>,
     <<"title">> => <<"EMQX MCP Bridge">>
 }).
 -define(MCP_MSG_HEADER, emqx_mcp_bridge).
